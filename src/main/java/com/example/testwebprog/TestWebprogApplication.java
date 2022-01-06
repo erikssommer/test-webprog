e3279328d361b4b6759e2000a5a50cf8e5481a7f
Erik Storås Sommer <erikssommer@gmail.com>
2022-01-06 11:19:49 +0100

    Initial commit

---
+++ src/main/java/com/example/testwebprog/TestWebprogApplication.java
@@ -0,0 +1,13 @@
+package com.example.testwebprog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWebprogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestWebprogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testwebprog/TestWebprogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testwebprog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestWebprogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
