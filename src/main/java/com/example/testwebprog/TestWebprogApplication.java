291f09b2368fd881be1e0791dd867ffbddf2682c
Erik Storås Sommer <erikssommer@gmail.com>
2022-01-06 11:22:56 +0100

    Forste commit fra intelliJ

--- src/main/java/com/example/testwebprog/TestWebprogApplication.java
+++ src/main/java/com/example/testwebprog/TestWebprogApplication.java
@@ -8,6 +8,7 @@ public class TestWebprogApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestWebprogApplication.class, args);
+        System.out.println("Hei fra oving");
     }
 
 }
